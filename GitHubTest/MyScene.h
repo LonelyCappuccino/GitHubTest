f2647e8debe8dc5d98de15882ee60ba465620a3a
Klemen Košir <klemenkosir@Klemens-Mac.local>
2013-09-13 14:13:28 +0200

    Initial Commit

---
+++ GitHubTest/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  GitHubTest
+//
+
+//  Copyright (c) 2013 Lonely Cappuccino. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
